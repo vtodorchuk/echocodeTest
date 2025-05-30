3937f0905f56a9ed06760a41ed1ea19e4d0ba9a9
vtodorchuk <bozodee8@gmail.com>
2025-05-30 13:06:04 +0300

    Initial Commit

---
+++ echocodeTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  echocodeTest
+//
+//  Created by Vlady Todorchuk on 30.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ echocodeTest/echocodeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  echocodeTestApp.swift
+//  echocodeTest
+//
+//  Created by Vlady Todorchuk on 30.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct echocodeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
