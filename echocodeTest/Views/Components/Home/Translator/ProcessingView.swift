96ff28f93e0d32a78bb80a78ceacd68d4b21587a
vtodorchuk <bozodee8@gmail.com>
2025-05-30 20:53:14 +0300

    Add ProcessingView

---
+++ echocodeTest/Views/Components/Home/Translator/ProcessingView.swift
@@ -0,0 +1,22 @@
+//
+//  ProcessingView.swift
+//  echocodeTest
+//
+//  Created by Vlady Todorchuk on 30.05.2025.
+//
+
+import SwiftUI
+
+struct ProcessingView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("Process of translation...")
+                .fontWeight(.semibold)
+        }
+    }
+}
+
+#Preview {
+    ProcessingView()
+}
